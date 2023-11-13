b442f9db53e3554f89c77a6e8dd23e69389bf5b3
marcelagorge <97238347+marcelagorge@users.noreply.github.com>
2023-11-12 21:27:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings:["preguicosa", "maluca", "fudida", "freddy fazbear"], 
+    strings:["Estudante", "Técnica de Informática", "Estagiária"], 
     typeSpeed: 100, 
     backSpeed: 100, 
     backDelay: 1000,
